89b8d380abbdbf7e91153d29f2e042f8c83fae44
riju377 <rijumondal377@gmail.com>
2023-07-09 11:58:02 +0530

    download fix10

--- circleciUpdate.py
+++ circleciUpdate.py
@@ -9,23 +9,6 @@ baseline_path = "/home/circleci/cypress-circleci-snapshot/cypress-visual-screens
 # Store the files present in the baseline folder before running npm command
 baseline_files_before = os.listdir(baseline_path)
 
-
-# Run the commands separately
-# subprocess.run("set +e", shell=True, executable="/bin/bash")
-# subprocess.run("npm run cy:run", shell=True, executable="/bin/bash")
-
-# # Check the exit status of tests
-# result = subprocess.run("echo $?", shell=True, executable="/bin/bash", capture_output=True)
-# exit_status = int(result.stdout.decode().strip())
-
-# # Handle the if condition
-# if exit_status != 0:
-#     # Perform actions when the condition is true
-#     subprocess.run("echo 'EXIT_CODE=1' >> $BASH_ENV", shell=True, executable="/bin/bash")
-
-# subprocess.run("set -e", shell=True, executable="/bin/bash")
-
-
 command = r'''
 set +e
 npm run cy:run
@@ -47,8 +30,6 @@ diff_files = os.listdir(diff_path)
 # Get the list of file names in the comparison folder
 comparison_files = os.listdir(comparison_path)
 
-# Get the updated list of file names in the baseline folder after running the npm command
-baseline_files_after = os.listdir(baseline_path)
 
 # Delete files in the comparison folder that don't have matching names in the diff folder
 for file_name in comparison_files:
